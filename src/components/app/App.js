9e95c9ede3073f6619be5ac063185f09dddb8303
Sergey <graygmai1@gmail.com>
2022-07-11 03:07:13 +0500

    api запрос на marvel

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,19 +3,31 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
+import MarvelService from "../../services/MarvelService";
+
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
+
+    // const marvelData = new MarvelService();
+
+    // marvelData.getAllCharacters()
+    //     .then(data => console.log(data))
+
+    // marvelData.getCharacter(1010915)
+    //     .then(data => console.log(data))
+
+
     return (
         <div className="app">
-            <AppHeader/>
+            <AppHeader />
             <main>
-                <RandomChar/>
+                <RandomChar />
                 <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
+                    <CharList />
+                    <CharInfo />
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <img className="bg-decoration" src={decoration} alt="vision" />
             </main>
         </div>
     )
